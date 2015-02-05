e58643f7b5aa1311082bec67bbb1b5becabe89d8
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2015-02-05 10:58:49 +0100

    copied relevant packages from the FPAI flexiblepower.api

---
+++ flexiblepower.api/src/org/flexiblepower/messaging/Endpoint.java
@@ -0,0 +1,12 @@
+package org.flexiblepower.messaging;
+
+public interface Endpoint {
+    /**
+     * Called when a new connection has been established.
+     *
+     * @param connection
+     *            The {@link Connection} object related to the connection.
+     * @return This method should return a reference to a
+     */
+    MessageHandler onConnect(Connection connection);
+}
