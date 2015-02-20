90bbba86e9c5c446ee99c89ebbed764b3980780b
Marc de Jonge <marc.dejonge@tno.nl>
2015-02-20 14:54:10 +0100

    Removed helper bundles, need to be evaluated first

--- flexiblepower.osgi/src/org/flexiblepower/osgi/configadmin/objects/Configuration.java
+++
@@ -1,9 +0,0 @@
-package org.flexiblepower.osgi.configadmin.objects;
-
-import java.util.HashMap;
-
-public class Configuration {
-    public String bundleId;
-    public String factoryId;
-    public HashMap<String, Object> properties;
-}
--- flexiblepower.osgi/src/org/flexiblepower/osgi/configadmin/objects/ConfigurationSet.java
+++
@@ -1,7 +0,0 @@
-package org.flexiblepower.osgi.configadmin.objects;
-
-import java.util.List;
-
-public class ConfigurationSet {
-    public List<Configuration> configurations;
-}
