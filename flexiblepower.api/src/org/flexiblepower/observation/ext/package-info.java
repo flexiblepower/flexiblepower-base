67dccfbdeefdedd5de29902060f7046c957be9a5
Marc de Jonge <marc.dejonge@tno.nl>
2015-02-20 15:54:15 +0100

    Added the Observation extensions

---
+++ flexiblepower.api/src/org/flexiblepower/observation/ext/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The extended observation package contains some helper implementations to make implementing an ObservationProvider
+ * much easier.
+ */
+@Version("2.0.0.${qualifier}")
+package org.flexiblepower.observation.ext;
+
+import aQute.bnd.annotation.Version;
+
